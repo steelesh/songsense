0b72bccbdee7175cf01d947c49462a27d6bda71b
Steele <shrevesa@mail.uc.edu>
2023-09-30 03:12:01 -0400

    feat(analytics): integrate Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="js">
 	import '../app.postcss';
 	import Navbar from '$lib/components/custom/navbar/navbar.svelte';
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <Navbar />
